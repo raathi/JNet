8f13a9eb0cd4c91e02d5dc0c6bec84f3de6cdc23
rtvasan08 <rtvasan08@gmail.com>
2013-11-04 14:45:44 +0530

    Added css and loaded ui with button action

--- JNet/src/com/ytdev/utils/jnet/JNetController.java
+++ JNet/src/com/ytdev/utils/jnet/JNetController.java
@@ -1,5 +1,362 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Thirumalaivasan Rajasekaran.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Thirumalaivasan Rajasekaran - initial API and implementation
+ ******************************************************************************/
 package com.ytdev.utils.jnet;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
+import javafx.stage.Stage;
+
 public class JNetController {
+	
+	@FXML
+	private Label appTitle;
+	
+	/*TCP*/
+	@FXML
+	private TextField txfIP_TCP;
+	@FXML
+	private TextField txfPort_TCP;
+	@FXML
+	private TextField txfMessage_TCP;
+	@FXML
+	private TextArea txaMessage_TCP;
+	@FXML
+	private ChoiceBox<JNetBean> chbSocketMode_TCP;
+	
+	/*UDP*/
+	@FXML
+	private TextField txfIP_UDP;
+	@FXML
+	private TextField txfPort_UDP;
+	@FXML
+	private TextField txfMessage_UDP;
+	@FXML
+	private TextArea txaMessage_UDP;
+	@FXML
+	private ChoiceBox<String> chbSocketMode_UDP;
+	@FXML
+	private RadioButton rdbMulticast;
+	@FXML
+	private RadioButton rdbUnicast;
+	
+	/*Network Interface*/
+	@FXML
+	private ProgressBar progressBarNWInterface;
+	@FXML
+	private TreeView<JNetBean> treViewNWInterface;
+	
+	/*System Information*/
+	@FXML
+	private ProgressBar progressBarSysInfo;
+	@FXML
+	private TableView<JNetBean> tblViewSysInfo;
+	
+	private Stage stage;
+	
+	
+	public void setStage(Stage stage){
+		this.stage = stage;
+	}
+	
+	@FXML
+	protected void sendTCPMessage(){
+		System.out.println("TCP Message Send"+txfIP_TCP.getText()+txfPort_TCP.getText());
+	}
+	
+	@FXML
+	protected void sendUDPMessage(){
+		System.out.println("UDP Message Send");
+	}
+	
+	@FXML
+	protected void clearTCPMsgHistory(){
+		txaMessage_TCP.clear();
+	}
+	
+	@FXML
+	protected void clearUDPMsgHistory(){
+		txaMessage_UDP.clear();
+	}
+	
+	@FXML
+	protected void processdistributionMode(){
+
+	}
+	
+	@FXML
+	protected void closeApp(){
+		stage.close();
+	}
+	
+	@FXML
+	protected void minimizeApp(){
+		stage.setIconified(true);
+	}
+	
+	@FXML
+	protected void aboutApp(){
+		System.out.println("JNet App version 0.01");
+	}
+
+	/**
+	 * @return the appTitle
+	 */
+	public final Label getAppTitle() {
+		return appTitle;
+	}
+
+	/**
+	 * @param appTitle the appTitle to set
+	 */
+	public final void setAppTitle(Label appTitle) {
+		this.appTitle = appTitle;
+	}
+
+	/**
+	 * @return the txfIP_TCP
+	 */
+	public final TextField getTxfIP_TCP() {
+		return txfIP_TCP;
+	}
+
+	/**
+	 * @param txfIP_TCP the txfIP_TCP to set
+	 */
+	public final void setTxfIP_TCP(TextField txfIP_TCP) {
+		this.txfIP_TCP = txfIP_TCP;
+	}
+
+	/**
+	 * @return the txfPort_TCP
+	 */
+	public final TextField getTxfPort_TCP() {
+		return txfPort_TCP;
+	}
+
+	/**
+	 * @param txfPort_TCP the txfPort_TCP to set
+	 */
+	public final void setTxfPort_TCP(TextField txfPort_TCP) {
+		this.txfPort_TCP = txfPort_TCP;
+	}
+
+	/**
+	 * @return the txfMessage_TCP
+	 */
+	public final TextField getTxfMessage_TCP() {
+		return txfMessage_TCP;
+	}
+
+	/**
+	 * @param txfMessage_TCP the txfMessage_TCP to set
+	 */
+	public final void setTxfMessage_TCP(TextField txfMessage_TCP) {
+		this.txfMessage_TCP = txfMessage_TCP;
+	}
+
+	/**
+	 * @return the txaMessage_TCP
+	 */
+	public final TextArea getTxaMessage_TCP() {
+		return txaMessage_TCP;
+	}
+
+	/**
+	 * @param txaMessage_TCP the txaMessage_TCP to set
+	 */
+	public final void setTxaMessage_TCP(TextArea txaMessage_TCP) {
+		this.txaMessage_TCP = txaMessage_TCP;
+	}
+
+	/**
+	 * @return the chbSocketMode_TCP
+	 */
+	public final ChoiceBox<JNetBean> getChbSocketMode_TCP() {
+		return chbSocketMode_TCP;
+	}
+
+	/**
+	 * @param chbSocketMode_TCP the chbSocketMode_TCP to set
+	 */
+	public final void setChbSocketMode_TCP(ChoiceBox<JNetBean> chbSocketMode_TCP) {
+		this.chbSocketMode_TCP = chbSocketMode_TCP;
+	}
+
+	/**
+	 * @return the txfIP_UDP
+	 */
+	public final TextField getTxfIP_UDP() {
+		return txfIP_UDP;
+	}
+
+	/**
+	 * @param txfIP_UDP the txfIP_UDP to set
+	 */
+	public final void setTxfIP_UDP(TextField txfIP_UDP) {
+		this.txfIP_UDP = txfIP_UDP;
+	}
+
+	/**
+	 * @return the txfPort_UDP
+	 */
+	public final TextField getTxfPort_UDP() {
+		return txfPort_UDP;
+	}
+
+	/**
+	 * @param txfPort_UDP the txfPort_UDP to set
+	 */
+	public final void setTxfPort_UDP(TextField txfPort_UDP) {
+		this.txfPort_UDP = txfPort_UDP;
+	}
+
+	/**
+	 * @return the txfMessage_UDP
+	 */
+	public final TextField getTxfMessage_UDP() {
+		return txfMessage_UDP;
+	}
+
+	/**
+	 * @param txfMessage_UDP the txfMessage_UDP to set
+	 */
+	public final void setTxfMessage_UDP(TextField txfMessage_UDP) {
+		this.txfMessage_UDP = txfMessage_UDP;
+	}
+
+	/**
+	 * @return the txaMessage_UDP
+	 */
+	public final TextArea getTxaMessage_UDP() {
+		return txaMessage_UDP;
+	}
+
+	/**
+	 * @param txaMessage_UDP the txaMessage_UDP to set
+	 */
+	public final void setTxaMessage_UDP(TextArea txaMessage_UDP) {
+		this.txaMessage_UDP = txaMessage_UDP;
+	}
+
+	/**
+	 * @return the chbSocketMode_UDP
+	 */
+	public final ChoiceBox<String> getChbSocketMode_UDP() {
+		return chbSocketMode_UDP;
+	}
+
+	/**
+	 * @param chbSocketMode_UDP the chbSocketMode_UDP to set
+	 */
+	public final void setChbSocketMode_UDP(ChoiceBox<String> chbSocketMode_UDP) {
+		this.chbSocketMode_UDP = chbSocketMode_UDP;
+	}
+
+	/**
+	 * @return the rdbMulticast
+	 */
+	public final RadioButton getRdbMulticast() {
+		return rdbMulticast;
+	}
+
+	/**
+	 * @param rdbMulticast the rdbMulticast to set
+	 */
+	public final void setRdbMulticast(RadioButton rdbMulticast) {
+		this.rdbMulticast = rdbMulticast;
+	}
+
+	/**
+	 * @return the rdbUnicast
+	 */
+	public final RadioButton getRdbUnicast() {
+		return rdbUnicast;
+	}
+
+	/**
+	 * @param rdbUnicast the rdbUnicast to set
+	 */
+	public final void setRdbUnicast(RadioButton rdbUnicast) {
+		this.rdbUnicast = rdbUnicast;
+	}
+
+	/**
+	 * @return the progressBarNWInterface
+	 */
+	public final ProgressBar getProgressBarNWInterface() {
+		return progressBarNWInterface;
+	}
+
+	/**
+	 * @param progressBarNWInterface the progressBarNWInterface to set
+	 */
+	public final void setProgressBarNWInterface(ProgressBar progressBarNWInterface) {
+		this.progressBarNWInterface = progressBarNWInterface;
+	}
+
+	/**
+	 * @return the treViewNWInterface
+	 */
+	public final TreeView<JNetBean> getTreViewNWInterface() {
+		return treViewNWInterface;
+	}
+
+	/**
+	 * @param treViewNWInterface the treViewNWInterface to set
+	 */
+	public final void setTreViewNWInterface(TreeView<JNetBean> treViewNWInterface) {
+		this.treViewNWInterface = treViewNWInterface;
+	}
+
+	/**
+	 * @return the progressBarSysInfo
+	 */
+	public final ProgressBar getProgressBarSysInfo() {
+		return progressBarSysInfo;
+	}
+
+	/**
+	 * @param progressBarSysInfo the progressBarSysInfo to set
+	 */
+	public final void setProgressBarSysInfo(ProgressBar progressBarSysInfo) {
+		this.progressBarSysInfo = progressBarSysInfo;
+	}
+
+	/**
+	 * @return the tblViewSysInfo
+	 */
+	public final TableView<JNetBean> getTblViewSysInfo() {
+		return tblViewSysInfo;
+	}
+
+	/**
+	 * @param tblViewSysInfo the tblViewSysInfo to set
+	 */
+	public final void setTblViewSysInfo(TableView<JNetBean> tblViewSysInfo) {
+		this.tblViewSysInfo = tblViewSysInfo;
+	}
+
+	/**
+	 * @return the stage
+	 */
+	public final Stage getStage() {
+		return stage;
+	}
+	
 	
 }
--- JNet/src/com/ytdev/utils/jnet/JNetLauncher.java
+++ JNet/src/com/ytdev/utils/jnet/JNetLauncher.java
@@ -1,21 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Thirumalaivasan Rajasekaran.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Thirumalaivasan Rajasekaran - initial API and implementation
+ ******************************************************************************/
 package com.ytdev.utils.jnet;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class JNetLauncher extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("JNet.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("JFxMetro.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("JNet.fxml"));
+			final Parent root = (Parent) fxmlLoader.load();
+			final JNetController controller = fxmlLoader.getController();
+			controller.setStage(stage);
+			controller.getProgressBarNWInterface().setPrefHeight(0.5);
+			Scene scene = new Scene(root,511,503);
+			scene.getStylesheets().add(getClass().getResource("JFxMetro_Rivulet.css").toExternalForm());
+			stage.initStyle(StageStyle.UNDECORATED);
+			stage.setTitle("JNet v0.01");		
+			stage.getIcons().add(new Image(getClass().getResourceAsStream( "JNet_Taskbar.png"))); 
+			stage.centerOnScreen();
+			stage.setScene(scene);
+			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
