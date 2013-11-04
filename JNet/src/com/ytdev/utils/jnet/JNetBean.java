3bbbbb3fc30e23b6507456dae121dfbf0c7f2b37
rtvasan08 <rtvasan08@gmail.com>
2013-11-04 14:48:57 +0530

    Added button image

---
+++ JNet/src/com/ytdev/utils/jnet/JNetBean.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ytdev.utils.jnet;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * @author Thirumalaivasan Rajasekaran
+ *
+ */
+public class JNetBean {
+	/*TCP*/
+	private final SimpleStringProperty socketModeTCP = new SimpleStringProperty();
+
+	/*UDP*/
+	private final SimpleStringProperty socketModeUDP = new SimpleStringProperty();
+	/*Network Interface*/
+	
+	/*Sytem Information*/
+}
